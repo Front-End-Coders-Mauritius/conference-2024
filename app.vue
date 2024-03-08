3d5c5b808b8d503dfa6533774564b9cf7fcd0a71
Kushul Soomaree <kushul@lsl.digital>
2024-03-08 20:44:17 +0400

    wip footer

--- app.vue
+++ app.vue
@@ -2,5 +2,6 @@
   <div>
     <Header />
     <NuxtWelcome />
+    <Footer />
   </div>
 </template>
---
+++ components/Footer.vue
@@ -0,0 +1,18 @@
+
+import type { Container } from '#build/components';
+<template>
+    <footer class="flex-none py-16">
+        <Container class="flex flex-col items-center justify-between md:flex-row">
+            <Logo class="h-12 w-auto text-slate-900" />
+            <p class="mt-6 text-base text-slate-500 md:mt-0">
+                Copyright &copy; {{ currentYear }} DeceptiConf, LLC. All rights reserved.
+            </p>
+        </Container>
+    </footer>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue';
+
+const currentYear = ref(new Date().getFullYear());
+</script>
