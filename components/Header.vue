c4b0ed975c21f79d7ae18ea43f93c6d07f9850f2
Sandeep Ramgolam <sandeep@ramgolam.com>
2024-04-04 00:59:54 +0400

    update sponsor logo

--- components/Header.vue
+++ components/Header.vue
@@ -8,8 +8,7 @@
                 class="order-first -mx-4 flex flex-auto basis-full overflow-x-auto whitespace-nowrap border-b border-blue-600/10 py-4 font-mono text-sm text-blue-600 sm:-mx-6 lg:order-none lg:mx-0 lg:basis-auto lg:border-0 lg:py-0">
                 <div class="mx-auto flex items-center gap-4 px-4">
                     <p>
-                        <time dateTime="2022-04-04">30</time>-
-                        <time dateTime="2022-04-06">31 of August, 2024</time>
+                        <time dateTime="2022-04-04">30</time>-<time dateTime="2022-04-06">31 of August, 2024</time>
                     </p>
                     <DiamondIcon className="h-1.5 w-1.5 overflow-visible fill-current stroke-current" />
                     <p>MDX, Mauritius</p>
--- components/Sponsors.vue
+++ components/Sponsors.vue
@@ -6,8 +6,8 @@
                 Current sponsorships for our workshops and speakers.
             </h2>
             <div
-                class="mx-auto mt-20 grid max-w-max grid-cols-1 place-content-center gap-x-32 gap-y-12 sm:grid-cols-3 md:gap-x-16 lg:gap-x-32">
-                <div v-for="sponsor in sponsors" :key="sponsor.name" class="flex items-center justify-center">
+                class="mx-auto mt-20 grid max-w-4xl grid-cols-1 place-content-center gap-x-32 gap-y-12 sm:grid-cols-3 md:gap-x-16 lg:gap-x-32">
+                <div v-for="sponsor in sponsors" :key="sponsor.name" class="flex items-center justify-center p-3 rounded-md" :class="sponsor.dar ? 'bg-black' : ''">
                     <nuxtImg :src="sponsor.logo" :alt="sponsor.name" unoptimized class="h-12" />
                 </div>
             </div>
@@ -17,11 +17,8 @@
 
 <script setup>
 const sponsors = [
-    { name: 'Transistor', logo: 'images/logos/transistor.svg' },
-    { name: 'Tuple', logo: 'images/logos/tuple.svg' },
-    { name: 'StaticKit', logo: 'images/logos/staticKit.svg' },
-    { name: 'Mirage', logo: 'images/logos/mirage.svg' },
-    { name: 'Laravel', logo: 'images/logos/laravel.svg' },
-    { name: 'Statamic', logo: 'images/logos/statamic.svg' },
+    { dar: true, name: 'MDX', logo: 'images/logos/mdx-logo.png' },
+    { dar: true, name: 'Honoris', logo: 'images/logos/honoris.webp' },
+    { dar: false, name: 'Upcode', logo: 'images/logos/upcode-log.svg' },
 ];
 </script>
