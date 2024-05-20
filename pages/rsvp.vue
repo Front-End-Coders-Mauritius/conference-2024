4c41e3367c1393a1ebc95ba497d6280a2ffd9b7c
mahima98 <mahimaramgolam98@gmail.com>
2024-05-20 14:39:56 +0400

    Added shadcn-vue

---
+++ pages/rsvp.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+
+import { useToast } from "@/components/ui/toast/use-toast";
+
+const { toast } = useToast();
+</script>
+
+<template>
+  <div class="flex justify-center items-center min-h-[90vh]">
+    <Button
+      variant="outline"
+      @click="
+        () => {
+          toast({
+            title: 'Scheduled: Catch up',
+            description: 'Friday, February 10, 2023 at 5:57 PM',
+          });
+        }
+      "
+    >
+      Add to calendar
+    </Button>
+  </div>
+</template>
