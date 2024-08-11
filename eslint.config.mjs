c5762e9664409dffc09fee9579ef261d8354127b
n-d-r-d-g <n.d.r.d.g@hotmail.com>
2024-08-11 20:44:55 +0400

    Added ESLint + VSCode configs

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+import antfu from '@antfu/eslint-config'
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt(
+  antfu({
+    vue: true,
+  }),
+  {
+    rules: {
+      'vue/first-attribute-linebreak': 'warn',
+      'vue/require-default-prop': 'off',
+      'vue/no-mutating-props': 'off',
+      'vue/no-template-shadow': 'off',
+      'vue/no-multiple-template-root': 'off',
+      'vue/max-attributes-per-line': 'warn',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      'unused-imports/no-unused-vars': 'warn',
+      'no-console': 'warn',
+      'array-callback-return': 'off',
+      'no-alert': 'off',
+    },
+  },
+)
