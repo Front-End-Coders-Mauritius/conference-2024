83b80bdb2244c287800716abc872bb3181da6fe4
n-d-r-d-g <n.d.r.d.g@hotmail.com>
2024-08-18 23:18:08 +0400

    Added agenda v1 with dummy data

--- components/Schedule.vue
+++ components/Schedule.vue
@@ -1,329 +1,241 @@
-<script setup lang="ts">
-import { onBeforeUnmount, onMounted, ref } from 'vue'
-import { Tab } from '@headlessui/vue'
-import clsx from 'clsx'
+<script lang="ts" setup>
+import customParseFormat from 'dayjs/plugin/customParseFormat'
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore'
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from '@/components/ui/tabs'
+import { Button } from '@/components/ui/button'
+import { useDayjs } from '#dayjs'
 
-interface TimeSlot {
+const rooms = ['room1', 'room2'] as const
+
+type Speaker = {
+  id: string
   name: string
-  description: string | null
-  start: string
-  end: string
+  avatar?: string
 }
 
-interface Day {
-  date: string
-  dateTime: string
-  summary: string
-  timeSlots: TimeSlot[]
+type Session = {
+  id: string
+  title: string
+  time: {
+    start: string
+    end: string
+  } // time should be in Mauritian time
+  speakers: Array<Speaker>
+  room?: typeof rooms[number]
+  isBookmarked?: boolean
 }
 
-const schedule = [
-  {
-    date: 'April 4',
-    dateTime: '2022-04-04',
-    summary:
-            'The first day of the conference is focused on dark patterns for ecommerce.',
-    timeSlots: [
-      {
-        name: 'Steven McHail',
-        description: 'Not so one-time payments',
-        start: '9:00AM',
-        end: '10:00AM',
-      },
-      {
-        name: 'Jaquelin Isch',
-        description: 'The finer print',
-        start: '10:00AM',
-        end: '11:00AM',
-      },
-      {
-        name: 'Dianne Guilianelli',
-        description: 'Post-purchase blackmail',
-        start: '11:00AM',
-        end: '12:00PM',
-      },
-      {
-        name: 'Lunch',
-        description: null,
-        start: '12:00PM',
-        end: '1:00PM',
-      },
-      {
-        name: 'Ronni Cantadore',
-        description: 'Buy or die',
-        start: '1:00PM',
-        end: '2:00PM',
-      },
-      {
-        name: 'Erhart Cockrin',
-        description: 'In-person cancellation',
-        start: '2:00PM',
-        end: '3:00PM',
-      },
-      {
-        name: 'Parker Johnson',
-        description: 'The pay/cancel switcheroo',
-        start: '3:00PM',
-        end: '4:00PM',
-      },
-    ],
-  },
-  {
-    date: 'April 5',
-    dateTime: '2022-04-05',
-    summary:
-            'Next we spend the day talking about deceiving people with technology.',
-    timeSlots: [
-      {
-        name: 'Damaris Kimura',
-        description: 'The invisible card reader',
-        start: '9:00AM',
-        end: '10:00AM',
-      },
-      {
-        name: 'Ibrahim Frasch',
-        description: 'Stealing fingerprints',
-        start: '10:00AM',
-        end: '11:00AM',
-      },
-      {
-        name: 'Cathlene Burrage',
-        description: 'Voting machines',
-        start: '11:00AM',
-        end: '12:00PM',
-      },
-      {
-        name: 'Lunch',
-        description: null,
-        start: '12:00PM',
-        end: '1:00PM',
-      },
-      {
-        name: 'Rinaldo Beynon',
-        description: 'Blackhat SEO that works',
-        start: '1:00PM',
-        end: '2:00PM',
-      },
-      {
-        name: 'Waylon Hyden',
-        description: 'Turning your audience into a botnet',
-        start: '2:00PM',
-        end: '3:00PM',
-      },
-      {
-        name: 'Giordano Sagucio',
-        description: 'Fly phishing',
-        start: '3:00PM',
-        end: '4:00PM',
-      },
-    ],
-  },
-  {
-    date: 'April 6',
-    dateTime: '2022-04-06',
-    summary:
-            'We close out the event previewing new techniques that are still in development.',
-    timeSlots: [
-      {
-        name: 'Andrew Greene',
-        description: 'Neuralink dark patterns',
-        start: '9:00AM',
-        end: '10:00AM',
-      },
-      {
-        name: 'Heather Terry',
-        description: 'DALL-E for passports',
-        start: '10:00AM',
-        end: '11:00AM',
-      },
-      {
-        name: 'Piers Wilkins',
-        description: 'Quantum password cracking',
-        start: '11:00AM',
-        end: '12:00PM',
-      },
-      {
-        name: 'Lunch',
-        description: null,
-        start: '12:00PM',
-        end: '1:00PM',
-      },
-      {
-        name: 'Gordon Sanderson',
-        description: 'SkyNet is coming',
-        start: '1:00PM',
-        end: '2:00PM',
-      },
-      {
-        name: 'Kimberly Parsons',
-        description: 'Dark patterns for the metaverse',
-        start: '2:00PM',
-        end: '3:00PM',
-      },
-      {
-        name: 'Richard Astley',
-        description: 'Knowing the game and playing it',
-        start: '3:00PM',
-        end: '4:00PM',
-      },
-    ],
-  },
-]
+type SessionsByDate = Record<string, Array<Session>>
 
-const tabOrientation = ref('horizontal')
+const dayjs = useDayjs()
+dayjs.extend(customParseFormat)
+dayjs.extend(isSameOrBefore)
 
-onMounted(() => {
-  const smMediaQuery = window.matchMedia('(min-width: 640px)')
+// NOTE: Sessions should be in ascending order of their start time
+const sessionsByDate = ref<SessionsByDate>({
+  '2024-09-03': [
+    {
+      id: 'a1',
+      title: 'Welcome to everyone',
+      time: {
+        start: '09:30',
+        end: '10:00',
+      },
+      speakers: [{ id: 'a1', name: 'paul' }],
+      room: 'room1',
+      isBookmarked: false,
+    },
+  ],
+  '2024-09-04': [
+    {
+      id: 'b1',
+      title: 'Google Developer Q&A',
+      time: {
+        start: '10:10',
+        end: '10:45',
+      },
+      speakers: [],
+      room: 'room1',
+      isBookmarked: false,
+    },
+    {
+      id: 'bz',
+      title: 'iOS Developer Q&A',
+      time: {
+        start: '10:10',
+        end: '10:30',
+      },
+      speakers: [],
+      room: 'room2',
+      isBookmarked: false,
+    },
+    {
+      id: 'b2',
+      title: 'Amazon Web Services',
+      time: {
+        start: '10:45',
+        end: '11:00',
+      },
+      speakers: [],
+      room: 'room1',
+      isBookmarked: false,
+    },
+    {
+      id: 'b3',
+      title: 'Intro to Vue.js',
+      time: {
+        start: '11:00',
+        end: '11:42',
+      },
+      speakers: [],
+      room: 'room2',
+      isBookmarked: false,
+    },
+    {
+      id: 'b4',
+      title: 'Intro to Vue.js',
+      time: {
+        start: '11:45',
+        end: '13:00',
+      },
+      speakers: [],
+      room: 'room2',
+      isBookmarked: false,
+    },
+  ],
+})
+const selectedDate = ref(Object.keys(sessionsByDate.value)[0])
 
-  const onMediaQueryChange = ({ matches }) => {
-    tabOrientation.value = matches ? 'vertical' : 'horizontal'
+const selectedDateSessions = computed(() => sessionsByDate.value[selectedDate.value])
+
+function getIntervalTime(time: string, intervalInMins: number, operation: 'add' | 'subtract') {
+  const parsedTime = dayjs(time, 'HH:mm')
+  const timeIntervalOffset = operation === 'add' ? intervalInMins - ((parsedTime.minute() % intervalInMins) || intervalInMins) : parsedTime.minute() % intervalInMins
+  const timeInterval = parsedTime[operation](timeIntervalOffset, 'minute')
+
+  return timeInterval
+}
+
+const intervalInMins = 15
+const timeintervals = computed(() => {
+  const startTime = selectedDateSessions.value[0].time.start
+  const endTime = selectedDateSessions.value.at(-1)?.time.end
+  const startTimeInterval = getIntervalTime(startTime, intervalInMins, 'subtract')
+  const endTimeInterval = getIntervalTime(endTime!, intervalInMins, 'add')
+  const intervals = []
+  let currentTimeInterval = startTimeInterval.clone()
+
+  while (currentTimeInterval.isSameOrBefore(endTimeInterval)) {
+    intervals.push(currentTimeInterval)
+    currentTimeInterval = currentTimeInterval.add(intervalInMins, 'minute')
   }
 
-  onMediaQueryChange(smMediaQuery)
-  smMediaQuery.addEventListener('change', onMediaQueryChange)
-
-  onBeforeUnmount(() => {
-    smMediaQuery.removeEventListener('change', onMediaQueryChange)
-  })
+  return intervals
 })
+
+const gridTemplateRows = computed(() => `auto ${[...Array(timeintervals.value.at(-1)!.diff(timeintervals.value[0], 'minute'))].map(() => 'auto').join(' ')}`)
+
+function getGridRow(time: string) {
+  const timeFromIntervalInMins = (dayjs(time, 'HH:mm').diff(timeintervals.value.at(0)!, 'minute'))
+  const rowOffset = 2
+  const gridRow = timeFromIntervalInMins + rowOffset
+
+  return gridRow
+}
+
+function getDay(date: string) {
+  return dayjs(date).utc().format('dddd')
+}
+
+function changeDay(newSelectedDate: string | number) {
+  selectedDate.value = newSelectedDate as string
+}
 </script>
 
 <template>
-  <section
-    aria-label="Schedule"
-    class="py-20 sm:py-32"
+  <Tabs
+    :default-value="Object.keys(sessionsByDate)[0]"
+    class="relative [--agenda-width:800px] w-[--agenda-width] max-w-full flex flex-col items-stretch gap-8 mx-auto overflow-x-auto rounded-md"
+    @update:model-value="changeDay"
   >
-    <Container class="relative z-10">
-      <div class="mx-auto max-w-2xl lg:mx-0 lg:max-w-4xl lg:pr-24">
-        <h2 class="font-display text-4xl font-medium tracking-tighter text-blue-600 sm:text-5xl">
-          Our three day schedule is jam-packed with brilliant, creative, evil geniuses.
-        </h2>
-        <p class="mt-4 font-display text-2xl tracking-tight text-blue-900">
-          The worst people in our industry giving the best talks you’ve ever seen.
-          Nothing will be recorded, and every attendee has to sign an NDA to watch the talks.
-        </p>
-      </div>
-    </Container>
-    <div class="relative mt-14 sm:mt-24">
-      <BackgroundImage
-        position="right"
-        class="-bottom-32 -top-40"
-      />
-      <Container class="relative">
-        <Tab.Group
-          as="div"
-          class="mx-auto grid max-w-2xl grid-cols-1 gap-y-6 sm:grid-cols-2 lg:hidden"
-          :vertical="tabOrientation === 'vertical'"
+    <TabsList class="justify-start max-w-full overflow-x-auto">
+      <TabsTrigger
+        v-for="date in Object.keys(sessionsByDate)"
+        :key="date"
+        :value="date"
+        class="grow"
+      >
+        {{ getDay(date) }}
+      </TabsTrigger>
+    </TabsList>
+    <TabsContent
+      :value="selectedDate"
+      class="grid gap-x-2 gap-y-1 max-h-[calc(100svh-48px)] px-1 pb-2 m-0 overflow-auto"
+      :style="{
+        gridTemplateRows,
+        gridTemplateColumns: `auto repeat(${rooms.length}, 1fr)`,
+      }"
+      tabindex="-1"
+    >
+      <div
+        class="sticky top-0 row-start-1 row-end-2 col-start-1 -col-end-1 mb-5 bg-green-200 rounded-md z-[3]"
+        aria-hidden="true"
+      ></div>
+      <p
+        v-for="(room, index) in rooms"
+        :key="room"
+        class="sticky top-0 row-start-1 row-end-2 mb-5 p-4 uppercase text-center rounded-sm z-[3]"
+        :style="{
+          gridColumnStart: index + 2,
+          gridColumnEnd: index + 3,
+        }"
+        aria-hidden="true"
+      >
+        {{ room }}
+      </p>
+      <template
+        v-for="(interval, index) in timeintervals"
+        :key="interval"
+      >
+        <p
+          class="sticky left-0.5 col-start-1 col-end-2 -mt-[0.9rem] h-6 px-1 py-0.5 font-mono rounded-sm bg-yellow-300 z-[2] before:absolute before:h-[2px] before:top-1/2 before:-translate-y-1/2 before:w-[calc(var(--agenda-width)-1rem)] before:bg-yellow-300/25 before:-z-[1] isolate"
+          :style="{
+            gridRowStart: (index * intervalInMins) + 2,
+            gridRowEnd: (index * intervalInMins) + 3,
+          }"
+          aria-hidden="true"
         >
-          <Tab.List
-            class="-mx-4 flex gap-x-4 gap-y-10 overflow-x-auto pb-4 pl-4 sm:mx-0 sm:flex-col sm:pb-0 sm:pl-0 sm:pr-8"
-          >
-            <div
-              v-for="(day, dayIndex) in schedule"
-              :key="day.dateTime"
-              :class="clsx('relative w-3/4 flex-none pr-4 sm:w-auto sm:pr-0', dayIndex !== selectedIndex && 'opacity-70')"
-            >
-              <h3 class="text-2xl font-semibold tracking-tight text-blue-900">
-                <time :dateTime="day.dateTime">{{ day.date }}</time>
-              </h3>
-              <p class="mt-1.5 text-base tracking-tight text-blue-900">
-                {{ day.summary }}
-              </p>
-            </div>
-          </Tab.List>
-          <Tab.Panels>
-            <Tab.Panel
-              v-for="day in schedule"
-              :key="day.dateTime"
-              class="ui-not-focus-visible:outline-none"
-            >
-              <ol
-                role="list"
-                class="space-y-8 bg-white/60 px-10 py-14 text-center shadow-xl shadow-blue-900/5 backdrop-blur"
-              >
-                <li
-                  v-for="(timeSlot, timeSlotIndex) in day.timeSlots"
-                  :key="timeSlot.start"
-                  aria-label="..."
-                >
-                  <div
-                    v-if="timeSlotIndex > 0"
-                    class="mx-auto mb-8 h-px w-48 bg-indigo-500/10"
-                  />
-                  <h4 class="text-lg font-semibold tracking-tight text-blue-900">
-                    {{ timeSlot.name }}
-                  </h4>
-                  <p
-                    v-if="timeSlot.description"
-                    class="mt-1 tracking-tight text-blue-900"
-                  >
-                    {{
-                      timeSlot.description }}
-                  </p>
-                  <p class="mt-1 font-mono text-sm text-slate-500">
-                    <time :dateTime="`${day.dateTime}T${timeSlot.start}-08:00`">
-                      {{ timeSlot.start }}
-                    </time>
-                    -
-                    <time :dateTime="`${day.dateTime}T${timeSlot.end}-08:00`">
-                      {{ timeSlot.end }}
-                    </time>
-                    PST
-                  </p>
-                </li>
-              </ol>
-            </Tab.Panel>
-          </Tab.Panels>
-        </Tab.Group>
-        <div class="hidden lg:grid lg:grid-cols-3 lg:gap-x-8">
-          <section
-            v-for="day in schedule"
-            :key="day.dateTime"
-          >
-            <h3 class="text-2xl font-semibold tracking-tight text-blue-900">
-              <time :dateTime="day.dateTime">{{ day.date }}</time>
-            </h3>
-            <p class="mt-1.5 text-base tracking-tight text-blue-900">
-              {{ day.summary }}
-            </p>
-            <ol
-              role="list"
-              class="space-y-8 bg-white/60 px-10 py-14 text-center shadow-xl shadow-blue-900/5 backdrop-blur mt-10"
-            >
-              <li
-                v-for="(timeSlot, timeSlotIndex) in day.timeSlots"
-                :key="timeSlot.start"
-                aria-label="..."
-              >
-                <div
-                  v-if="timeSlotIndex > 0"
-                  class="mx-auto mb-8 h-px w-48 bg-indigo-500/10"
-                />
-                <h4 class="text-lg font-semibold tracking-tight text-blue-900">
-                  {{ timeSlot.name }}
-                </h4>
-                <p
-                  v-if="timeSlot.description"
-                  class="mt-1 tracking-tight text-blue-900"
-                >
-                  {{
-                    timeSlot.description }}
-                </p>
-                <p class="mt-1 font-mono text-sm text-slate-500">
-                  <time :dateTime="`${day.dateTime}T${timeSlot.start}-08:00`">
-                    {{ timeSlot.start }}
-                  </time>
-                  -
-                  <time :dateTime="`${day.dateTime}T${timeSlot.end}-08:00`">
-                    {{ timeSlot.end }}
-                  </time>
-                  PST
-                </p>
-              </li>
-            </ol>
-          </section>
-        </div>
-      </Container>
-    </div>
-  </section>
+          {{ interval.format('HH:mm') }}
+        </p>
+      </template>
+      <Button
+        v-for="session in selectedDateSessions"
+        :key="session.id"
+        type="button"
+        variant="outline"
+        class="h-auto flex flex-col justify-start items-start p-3 rounded-lg"
+        :style="{
+          gridRowStart: getGridRow(session.time.start),
+          gridRowEnd: getGridRow(session.time.end),
+          gridColumnStart: session.room ? rooms.indexOf(session.room) + 2 : 2,
+          gridColumnEnd: session.room ? rooms.indexOf(session.room) + 3 : -1,
+        }"
+      >
+        <p class="z-[1]">
+          {{ session.time.start }} - {{ session.time.end }}
+        </p>
+        <p class="z-[1]">
+          {{ session.title }}
+        </p>
+        <p class="z-[1]">
+          other info
+        </p>
+      </Button>
+    </TabsContent>
+  </Tabs>
 </template>
