9526be6faa2f1be4e3025fc4aee4ff1413624ed9
n-d-r-d-g <n.d.r.d.g@hotmail.com>
2024-08-20 17:08:13 +0400

    Fix attempt for nested NuxtLink warning

--- components/agenda/session/speaker/index.vue
+++ components/agenda/session/speaker/index.vue
@@ -1,5 +1,26 @@
+<script lang="ts" setup>
+const props = defineProps({
+  href: {
+    required: true,
+    type: String,
+  },
+})
+
+const router = useRouter()
+
+function navigateToHref() {
+  router.push(props.href)
+}
+</script>
+
 <template>
-  <NuxtLink>
+  <span
+    :data-href="href"
+    tabindex="0"
+    role="link"
+    @click.prevent="navigateToHref"
+    @keyup.enter.prevent="navigateToHref"
+  >
     <slot></slot>
-  </NuxtLink>
+  </span>
 </template>
