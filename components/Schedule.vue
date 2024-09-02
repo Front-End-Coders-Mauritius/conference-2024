fbd35da71c1223e57216f612128c97b07be8fee9
n-d-r-d-g <n.d.r.d.g@hotmail.com>
2024-09-02 20:04:53 +0400

    Added current time indicator on agenda

--- components/Schedule.vue
+++ components/Schedule.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import customParseFormat from 'dayjs/plugin/customParseFormat'
 import isSameOrBefore from 'dayjs/plugin/isSameOrBefore'
+import isBetween from 'dayjs/plugin/isBetween'
 import { useElementSize } from '@vueuse/core'
 import {
   Tabs,
@@ -24,21 +25,29 @@ const props = defineProps({
 const dayjs = useDayjs()
 dayjs.extend(customParseFormat)
 dayjs.extend(isSameOrBefore)
+dayjs.extend(isBetween)
 
+const currentDateTime = ref(dayjs())
+const agendaContentRef = ref(null)
 const roomsContainerRef = ref(null)
 const { width: roomsContainerWidth } = useElementSize(roomsContainerRef)
 const sessionsByDate = ref(SESSIONS_BY_DATE)
 const selectedDate = ref(Object.keys(sessionsByDate.value)[0] as SessionDate)
 const bookmarkedSessions = ref(new Map<string, Session>())
+let interval: undefined | number
 
 const selectedDateSessions = computed(() => sessionsByDate.value[selectedDate.value] as Array<Session>)
 
 onMounted(() => {
   bookmarkedSessions.value = new Map(JSON.parse(localStorage.getItem(BOOKMARKED_SESSIONS_STORAGE_KEY)!))
+
+  interval = window?.setInterval(() => currentDateTime.value = dayjs(), 1000)
 })
 
-function getIntervalTime(time: string, intervalInMins: number, operation: 'add' | 'subtract') {
-  const parsedTime = dayjs(time, 'HH:mm')
+onBeforeMount(() => window?.clearInterval(interval))
+
+function getIntervalDateTime(date: string, time: string, intervalInMins: number, operation: 'add' | 'subtract') {
+  const parsedTime = dayjs(`${date}T${time}`, 'YYYY-MM-DDTHH:mm')
   const timeIntervalOffset = operation === 'add' ? intervalInMins - ((parsedTime.minute() % intervalInMins) || intervalInMins) : parsedTime.minute() % intervalInMins
   const timeInterval = parsedTime[operation](timeIntervalOffset, 'minute')
 
@@ -48,8 +57,8 @@ function getIntervalTime(time: string, intervalInMins: number, operation: 'add' 
 const timeintervals = computed(() => {
   const startTime = selectedDateSessions.value[0].time.start
   const endTime = selectedDateSessions.value.at(-1)?.time.end
-  const startTimeInterval = getIntervalTime(startTime, props.intervalInMins, 'subtract')
-  const endTimeInterval = getIntervalTime(endTime!, props.intervalInMins, 'add')
+  const startTimeInterval = getIntervalDateTime(selectedDate.value, startTime, props.intervalInMins, 'subtract')
+  const endTimeInterval = getIntervalDateTime(selectedDate.value, endTime!, props.intervalInMins, 'add')
   const intervals = []
   let currentTimeInterval = startTimeInterval.clone()
 
@@ -61,8 +70,19 @@ const timeintervals = computed(() => {
   return intervals
 })
 
-function getGridRow(time: string) {
-  const timeFromIntervalInMins = (dayjs(time, 'HH:mm').diff(timeintervals.value.at(0)!, 'minute'))
+const currentTimeGridRow = computed(() => {
+  const agendaStartDateTime = timeintervals.value.at(0)
+  const agendaEndDateTime = timeintervals.value.at(-1)
+  const isCurrentDateInAgenda = currentDateTime.value.isBetween(agendaStartDateTime, agendaEndDateTime, null, '[]')
+
+  if (!isCurrentDateInAgenda)
+    return null
+
+  return getGridRow(selectedDate.value, currentDateTime.value.format('HH:mm'))
+})
+
+function getGridRow(date: string, time: string) {
+  const timeFromIntervalInMins = (dayjs(`${date}T${time}`, 'YYYY-MM-DDTHH:mm').diff(timeintervals.value.at(0)!, 'minute'))
   const rowOffset = 2
   const gridRow = timeFromIntervalInMins + rowOffset
 
@@ -70,7 +90,7 @@ function getGridRow(time: string) {
 }
 
 function getDay(date: string) {
-  return dayjs(date).utc().format('dddd')
+  return dayjs(date).format('dddd')
 }
 
 function changeDay(newSelectedDate: string | number) {
@@ -94,117 +114,142 @@ function isSessionBookmarked(sessionId: string) {
 </script>
 
 <template>
-  <Tabs
-    :default-value="Object.keys(sessionsByDate)[0]"
-    class="relative [--agenda-width:800px] w-[--agenda-width] max-w-full flex flex-col items-stretch gap-8 mx-auto overflow-x-auto rounded-md"
-    @update:model-value="changeDay"
-  >
-    <TabsList class="justify-start max-w-full overflow-x-auto">
-      <TabsTrigger
-        v-for="date in Object.keys(sessionsByDate)"
-        :key="date"
-        :value="date"
-        class="grow"
-      >
-        {{ getDay(date) }}
-      </TabsTrigger>
-    </TabsList>
-    <TabsContent
-      :value="selectedDate"
-      class="grid gap-x-2 gap-y-1 max-h-[calc(100svh-48px)] px-1 pb-2 m-0 overflow-auto"
-      :style="{
-        gridTemplateRows: `auto repeat(${timeintervals.at(-1)!.diff(timeintervals[0], 'minute')}, auto)`,
-        gridTemplateColumns: `auto repeat(${ROOMS.length}, minmax(16rem,1fr))`,
-      }"
-      tabindex="-1"
+  <div class="bg-white py-8 rounded-lg">
+    <Tabs
+      :default-value="Object.keys(sessionsByDate)[0]"
+      class="relative [--agenda-width:800px] w-[--agenda-width] max-w-full flex flex-col items-stretch gap-8 mx-auto overflow-x-auto rounded-md"
+      @update:model-value="changeDay"
     >
-      <div
-        ref="roomsContainerRef"
-        class="sticky top-0 row-start-1 row-end-2 col-start-1 -col-end-1 mb-5 bg-white rounded-md shadow-lg z-[3]"
-        aria-hidden="true"
-      ></div>
-      <AgendaRoom
-        v-for="(room, index) in ROOMS"
-        :key="room"
-        class="sticky top-0 row-start-1 row-end-2 mb-5 p-2 uppercase text-md text-center text-blue-800 rounded-sm z-[3]"
-        :style="{
-          gridColumnStart: index + 2,
-          gridColumnEnd: index + 3,
-        }"
-      >
-        {{ room }}
-      </AgendaRoom>
-      <AgendaTimeInterval
-        v-for="(interval, index) in timeintervals"
-        :key="interval.format('HH:mm')"
-        class="sticky left-0.5 col-start-1 col-end-2 -mt-[0.9rem] h-6 px-1 py-0.5 font-mono rounded-sm bg-yellow-300 z-[2]"
-        :style="{
-          gridRowStart: (index * intervalInMins) + 2,
-          gridRowEnd: (index * intervalInMins) + 3,
-        }"
-      >
-        {{ interval.format('HH:mm') }}
-        <AgendaTimeIntervalLine
-          class="absolute [--height:2px] h-[--height] left-100% top-1/2 -translate-y-1/2 bg-yellow-300/50"
-          :style="{ width: `calc(${roomsContainerWidth}px - 100%)` }"
-        />
-      </AgendaTimeInterval>
-      <AgendaSession
-        v-for="session in selectedDateSessions"
-        :key="session.id"
-        :data-state="isSessionBookmarked(session.id) ? 'bookmarked' : 'not-bookmarked'"
-        :href="`/session/${session.id}`"
-        :link-class="cn(
-          buttonVariants({ variant: 'outline' }),
-          'h-auto p-0 rounded-lg hover:bg-gray-50 hover:shadow-lg focus-visible:bg-gray-50 focus-visible:shadow-lg motion-safe:transition',
-          isSessionBookmarked(session.id) && 'outline-dashed outline-4 -outline-offset-4 outline-green-700',
-        )"
-        class="relative h-auto flex flex-col justify-start items-start gap-1 p-3 text-sm rounded-lg"
-        :style="{
-          gridRowStart: getGridRow(session.time.start),
-          gridRowEnd: getGridRow(session.time.end),
-          gridColumnStart: session.room ? ROOMS.indexOf(session.room) + 2 : 2,
-          gridColumnEnd: session.room ? ROOMS.indexOf(session.room) + 3 : -1,
-        }"
-      >
-        <AgendaSessionMeta
-          :start-time="session.time.start"
-          :end-time="session.time.end"
-          :location="session.room?.toUpperCase()"
-          class="text-gray-600 z-[1]"
-        />
-        <AgendaSessionTitle class="text-[1rem] text-wrap z-[1]">
-          {{ session.title }}
-        </AgendaSessionTitle>
-        <AgendaSessionSpeakers
-          v-if="session.speakers.length > 0"
-          class="flex flex-col gap-1 mt-2"
+      <TabsList class="max-w-full justify-start bg-gray-50 overflow-x-auto">
+        <TabsTrigger
+          v-for="date in Object.keys(sessionsByDate)"
+          :key="date"
+          :value="date"
+          class="grow data-[active=true]:text-white data-[active=true]:bg-blue-900 hover:data-[active=false]:bg-gray-100 motion-safe:transition-all"
         >
-          <AgendaSessionSpeaker
-            v-for="speaker in session.speakers"
-            :key="speaker.id"
-            :href="`/speaker/${speaker.id}`"
-            :class="cn(buttonVariants({ variant: 'ghost' }), 'h-fit flex flex-row items-center gap-1 p-0 z-[1] hover:bg-transparent hover:scale-105 motion-safe:transition-all')"
-          >
-            <AgendaSessionSpeakerAvatar
-              :src="speaker.avatar ?? ''"
-              class="h-[32px] self-start aspect-square rounded-full text-transparent bg-slate-200"
-            />
-            <AgendaSessionSpeakerName class="text-wrap">
-              {{ speaker.name }}
-            </AgendaSessionSpeakerName>
-          </AgendaSessionSpeaker>
-        </AgendaSessionSpeakers>
-        <AgendaSessionBookmarkButton
-          :class="cn(
-            isSessionBookmarked(session.id) && 'text-white bg-green-800',
-            'mt-2 outline outline-1 outline-gray-200 hover:text-white hover:bg-black focus-visible:text-white focus-visible:bg-black z-[1]',
+          {{ getDay(date) }}
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent
+        ref="agendaContentRef"
+        :value="selectedDate"
+        class="grid gap-x-2 gap-y-1 max-h-[calc(100svh-48px)] px-1 pb-2 m-0 overflow-auto"
+        :style="{
+          gridTemplateRows: `auto repeat(${timeintervals.at(-1)!.diff(timeintervals[0], 'minute')}, auto)`,
+          gridTemplateColumns: `auto repeat(${ROOMS.length}, minmax(16rem,1fr))`,
+        }"
+        tabindex="-1"
+      >
+        <div
+          ref="roomsContainerRef"
+          class="sticky top-0 row-start-1 row-end-2 col-start-1 -col-end-1 mb-5 bg-white border border-gray-100 rounded-md shadow-md z-[3]"
+          aria-hidden="true"
+        ></div>
+        <AgendaRoom
+          v-for="(room, index) in ROOMS"
+          :key="room"
+          class="sticky top-0 row-start-1 row-end-2 mb-5 p-2 uppercase text-md text-center text-blue-900 rounded-md z-[3]"
+          :style="{
+            gridColumnStart: index + 2,
+            gridColumnEnd: index + 3,
+          }"
+        >
+          {{ room }}
+        </AgendaRoom>
+        <AgendaTimeInterval
+          v-for="(interval, index) in timeintervals"
+          :key="interval.format('HH:mm')"
+          class="sticky left-0.5 col-start-1 col-end-2 -mt-[0.9rem] h-6 px-1 py-0.5 font-mono rounded-sm bg-yellow-300 z-[2]"
+          :style="{
+            gridRowStart: (index * intervalInMins) + 2,
+            gridRowEnd: (index * intervalInMins) + 3,
+          }"
+        >
+          {{ interval.format('HH:mm') }}
+          <AgendaTimeIntervalLine
+            class="absolute [--height:2px] h-[--height] left-100% top-1/2 -translate-y-1/2 bg-yellow-300/50 pointer-events-none"
+            :style="{ width: `calc(${roomsContainerWidth}px - 100%)` }"
+          />
+        </AgendaTimeInterval>
+        <AgendaSession
+          v-for="session in selectedDateSessions"
+          :key="session.id"
+          :data-state="isSessionBookmarked(session.id) ? 'bookmarked' : 'not-bookmarked'"
+          :href="`/session/${session.id}`"
+          :link-class="cn(
+            buttonVariants({ variant: 'outline' }),
+            'h-auto p-0 rounded-lg bg-gray-50/25 hover:bg-gray-50 hover:shadow-lg focus-visible:bg-gray-50 focus-visible:shadow-lg motion-safe:transition',
+            isSessionBookmarked(session.id) && 'outline-dashed outline-4 -outline-offset-4 outline-green-700',
           )"
-          @click.prevent="toggleBookmark(session)"
+          class="relative h-auto flex flex-col justify-start items-start gap-1 p-3 text-sm rounded-lg"
+          :style="{
+            gridRowStart: getGridRow(selectedDate, session.time.start),
+            gridRowEnd: getGridRow(selectedDate, session.time.end),
+            gridColumnStart: session.room ? ROOMS.indexOf(session.room) + 2 : 2,
+            gridColumnEnd: session.room ? ROOMS.indexOf(session.room) + 3 : -1,
+          }"
         >
-          {{ isSessionBookmarked(session.id) ? 'Bookmarked' : 'Bookmark' }}
-        </AgendaSessionBookmarkButton>
-      </AgendaSession>
-    </TabsContent>
-  </Tabs>
+          <AgendaSessionMeta
+            :start-time="session.time.start"
+            :end-time="session.time.end"
+            :location="session.room?.toUpperCase()"
+            class="text-gray-600 z-[1]"
+          />
+          <AgendaSessionTitle class="text-[1rem] text-wrap z-[1]">
+            {{ session.title }}
+          </AgendaSessionTitle>
+          <AgendaSessionSpeakers
+            v-if="session.speakers.length > 0"
+            class="flex flex-col gap-1 mt-2"
+          >
+            <AgendaSessionSpeaker
+              v-for="speaker in session.speakers"
+              :key="speaker.id"
+              :href="`/speaker/${speaker.id}`"
+              :class="cn(buttonVariants({ variant: 'ghost' }), 'h-fit flex flex-row items-center gap-1 p-0 z-[1] hover:bg-transparent hover:scale-105 motion-safe:transition-all')"
+            >
+              <AgendaSessionSpeakerAvatar
+                :src="speaker.avatar ?? ''"
+                class="h-[32px] self-start aspect-square rounded-full text-transparent bg-slate-200"
+              />
+              <AgendaSessionSpeakerName class="text-wrap">
+                {{ speaker.name }}
+              </AgendaSessionSpeakerName>
+            </AgendaSessionSpeaker>
+          </AgendaSessionSpeakers>
+          <AgendaSessionBookmarkButton
+            :class="cn(
+              isSessionBookmarked(session.id) && 'text-white bg-green-800',
+              'mt-2 outline outline-1 outline-gray-200 hover:text-white hover:bg-black focus-visible:text-white focus-visible:bg-black z-[1]',
+            )"
+            @click.prevent="toggleBookmark(session)"
+          >
+            {{ isSessionBookmarked(session.id) ? 'Bookmarked' : 'Bookmark' }}
+          </AgendaSessionBookmarkButton>
+        </AgendaSession>
+        <AgendaCurrentTime
+          v-if="currentTimeGridRow"
+          class="relative col-start-1 -col-end-1 z-[2] motion-safe:transition-all"
+          :style="{
+            gridRowStart: currentTimeGridRow,
+            gridRowEnd: currentTimeGridRow + 1,
+          }"
+        >
+          <AgendaCurrentTimeLine
+            :current-date-time="currentDateTime"
+            class="absolute [--height:4px] h-[--height] -top-[--height] right-0 -translate-y-1/2 bg-red-500/50 cursor-pointer hover:h-[12px] hover:-top-[8px] hover:bg-red-500/30 motion-safe:transition-all"
+            :style="{
+              width: `${roomsContainerWidth}px`,
+            }"
+          />
+          <AgendaCurrentTimeOverlay
+            class="absolute h-[100000vmax] right-0 bottom-0 bg-red-400/5 pointer-events-none"
+            :style="{
+              width: `${roomsContainerWidth}px`,
+            }"
+          />
+        </AgendaCurrentTime>
+      </TabsContent>
+    </Tabs>
+  </div>
 </template>
